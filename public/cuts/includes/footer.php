545a5114fde2998f32431bf97c7da03e2f302576
Tom Bates <tom@soundintheory.co.uk>
2015-11-22 17:39:48 +0000

    Added visual aid to label breakpoints in cuts

--- public/cuts/includes/footer.php
+++ public/cuts/includes/footer.php
@@ -1,3 +1,12 @@
 <?php include('demo.php'); ?>	
+	<!-- Helper to show which breakpoint is visible -->
+	<span class="helper" style="position: absolute; bottom: 0px; right: 0px; background: rgba(0,0,0,0.8); margin: 0px; font-size: 16px; padding: 5px; color: #fff" >
+		<span class="visible-lg-inline">Large (lg)</span>
+		<span class="visible-md-inline">Desktop (md)</span>
+		<span class="visible-sm-inline">Tablet (sm)</span>
+		<span class="visible-xs-inline">Handheld Landscape (xs)</span>
+		<span class="visible-hs-inline">Handheld (hs)</span>
+	</span>
+	<!-- END Helper -->
 </body>
 </html>
