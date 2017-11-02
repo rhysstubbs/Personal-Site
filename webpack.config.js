5dc354e5e5c9b4d7deb01aef224739168233d063
Rhys Stubbs <rhys@soundintheory.co.uk>
2017-11-02 11:51:58 +0000

    UI updates and fix for repeating warning box javascript.

--- webpack.config.js
+++ webpack.config.js
@@ -1,89 +1,34 @@
 const webpack = require('webpack'); //to access built-in plugins
 const path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+var glob = require("glob");
 
-module.exports = {
-    entry: {
-        index: './assets/index.js',
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, '/assets')
-    }
-}
-
-
-
-/**
-module.exports = {
-    entry: './public/assets/js/core.js',
-    output: {
-        path: path.resolve(__dirname, 'public/assets/js/dist'),
-        filename: 'core.min.js'
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            sourceMap: true
-        })
-    ]
-};
-
-
-module.exports = {
-    entry: './public/assets/scss/screen.scss',
+var configuration = {
     devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "sass-loader?sourceMap"
-                    }, {
-                        loader: "resolve-url-loader"
-                    }, {
-                        loader: "file-loader"
-                    }],
-                    fallback:'style-loader',
-                })
-
-            }
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, './public/assets/css/'),
-        filename: 'test.min.css'
-    },
-    plugins: [
-        new ExtractTextPlugin('./public/assets/scss/screen.scss')
-    ]
-};
-
-
-module.exports = {
     entry: {
-        jquery: "./node_modules/jquery/dist/jquery.js",
-        html5shiv: "./node_modules/html5shiv/dist/html5shiv.js",
-        bootstrap_sass: "./node_modules/bootstrap-sass/assets/javascripts/bootstrap",
-        parsleyjs: "./node_modules/parsleyjs/dist/parsley.js",
-        jquery_placeholder: "./node_modules/jquery-placeholder/jquery.placeholder.js",
-        imagesloaded: "./node_modules/imagesloaded/imagesloaded.js",
-        magnific_popup: "./node_modules/magnific-popup/dist/jquery.magnific-popup.js",
-        slick_slider: "./node_modules/slick-carousel/slick/slick.js"
+        app: glob.sync('./public/assets/js/**/*.js'),
+        vendor: [
+            'jquery',
+            'jquery-placeholder',
+            'html5shiv',
+            'parsleyjs',
+            'slick-carousel',
+            'magnific-popup'
+        ],
     },
     output: {
-        path: path.resolve(__dirname, 'public/assets/js/dist'),
-        filename: 'test.min.js'
+        path: path.resolve(__dirname, 'public/assets/'),
+        filename: '[name].bundle.min.js'
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
+            parallel: true,
+            sourceMap: true,
             minimize: true,
-            sourceMap: true
+            comments: false
         })
     ]
 };
-*/
+
+module.exports = configuration;
